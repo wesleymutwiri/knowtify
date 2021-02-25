d85a978a4aaac92aad0b633882d13939f45474d1
wesleymutwiri <wesleymochiemo@gmail.com>
2021-02-25 21:19:58 +0300

    recreated site using vue cli

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div id="app">
+    <Navbar />
+    <router-view />
+  </div>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+<script>
+import Navbar from "@/components/Navbar";
+export default {
+  components: {
+    Navbar
+  }
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
