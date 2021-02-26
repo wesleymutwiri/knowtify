ca83745953a96ecebb3051289de892d8c3a2845c
wesleymutwiri <wesleymochiemo@gmail.com>
2021-02-27 00:29:42 +0300

    adding firebase to project

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,20 @@
 </template>
 
 <script>
+import Firebase from "firebase";
 import Navbar from "@/components/Navbar";
+const app = Firebase.initializeApp(process.env.config);
+const db = app.database();
+
+const notes = db.ref("notes");
+
 export default {
+  name: "app",
   components: {
     Navbar
+  },
+  firebase: {
+    notes: notes
   }
 };
 </script>
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import { firestorePlugin } from "vuefire";
 
 Vue.config.productionTip = false;
+
+Vue.use(firestorePlugin);
 
 new Vue({
   router,
