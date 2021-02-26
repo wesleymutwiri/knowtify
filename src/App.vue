a25ee99c0b68fa9344ffac4eb3eedcb223c90ef8
wesleymutwiri <wesleymochiemo@gmail.com>
2021-02-26 16:28:09 +0300

    implemented basic tiptap editor

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div id="app">
     <Navbar />
+    <Sidebar />
     <router-view />
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 export default {
   components: {
-    Navbar
+    Navbar,
+    Sidebar
   }
 };
 </script>
+
+<style scoped></style>
---
+++ src/views/Editor.vue
@@ -0,0 +1,36 @@
+<template>
+  <editor-content :editor="editor" />
+</template>
+
+<script>
+import { Editor, EditorContent } from "tiptap";
+import { Bold, Italic, Link, HardBreak, Heading } from "tiptap-extensions";
+
+export default {
+  name: "Editor",
+  components: {
+    EditorContent
+  },
+  data() {
+    return {
+      editor: new Editor({
+        extensions: [
+          new Bold(),
+          new Italic(),
+          new Link(),
+          new HardBreak(),
+          new Heading()
+        ],
+        onUpdate: ({ getHTML }) => {
+          const newContent = getHTML();
+        },
+        content: "<p> Some boring paragraph </p>"
+      })
+    };
+  },
+  beforeDestroy() {
+    // Always destroy the editor instance when no longer needed
+    this.editor.destroy();
+  }
+};
+</script>
