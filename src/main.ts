ea6b5d75655bcfeecb8723aa45344f7475afbbd5
wesleymutwiri <wesleymochiemo@gmail.com>
2021-03-02 22:28:13 +0300

    added firebase to project and set up login and signup basically

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,25 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import { firestorePlugin } from "vuefire";
+import firebase from "firebase";
 
 Vue.config.productionTip = false;
+
+const config = {
+  apiKey: "AIzaSyDo2Co51ezdg-W0rtnKTmD6h9Q6RwBrpNc",
+  authDomain: "knowtify-io.firebaseapp.com",
+  databaseURL:
+    "https://knowtify-io-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "knowtify-io",
+  storageBucket: "knowtify-io.appspot.com",
+  messagingSenderId: "408114689085",
+  appId: "1:408114689085:web:f070a770106e57e1b9e654",
+  measurementId: "G-626SYTFZ0W"
+};
+
+console.log(process.env.APIKEY);
+
+firebase.initializeApp(config);
 
 Vue.use(firestorePlugin);
 
---
+++ src/views/Login.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <div class="form-image">
+      <img src="" alt="" />
+    </div>
+    <div class="form">
+      <h1>Login</h1>
+      <form v-on:submit.prevent>
+        <input type="email" name="email" id="" v-model="email" />
+        <input type="password" name="password" id="" v-model="password" />
+        <button class="login" @click="login">Login</button>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+import firebase from "firebase";
+
+export default {
+  name: "login",
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    login() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(userCredential => {
+          alert("Successfully logged in", userCredential);
+          this.$router.replace("/");
+        })
+        .catch(error => {
+          alert("Something happened", error);
+        });
+    }
+  }
+};
+</script>
